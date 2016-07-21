0f388e6ba0223c3e3cb4bf86402ef1ce2f9d9cd6
Timothy Stephens <tstephens@handy.com>
2016-07-21 17:38:17 -0400

    No ES6

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-let express = require('express'),
+var express = require('express'),
     firebase = require('firebase'),
     Promise = require('bluebird'),
     app = express();
 
-app.get('/', (req, res) => {
+app.get('/', function(req, res) {
     res.send('Hello world2');
 });
 
-app.listen(3000, () => {
+app.listen(3000, function() {
     console.log('Express listening on port 3000');
 });
