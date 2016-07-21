cd874afbebc8f4ad8b87b77d3418fd1eaaa574fa
Timothy Stephens <tstephens@handy.com>
2016-07-21 17:45:47 -0400

    Added POST endpoint

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 var express = require('express'),
     firebase = require('firebase'),
     Promise = require('bluebird'),
-    app = express();
+    app = express(),
+    SLACK_TOKEN = 'mDaXXt5QKddzhdQ5pA5cjBdH';
 
 process.env.PORT = process.env.PORT || 3000;
 
 app.get('/', function(req, res) {
-    res.send('Hello world2');
+    res.send('Hello foos!');
+});
+
+app.post('/', function(req, res) {
+    if(req.body.token != SLACK_TOKEN) {
+        res.status(404).send("Invalid token");
+    } else {
+        res.send(req.body);
+    }
 });
 
 app.listen(process.env.PORT, function() {
